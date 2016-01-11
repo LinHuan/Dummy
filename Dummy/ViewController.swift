54d071d42c18981e2a33e85f33226a3c1ea5d858
Huan Lin <lifesaglitch@qq.com>
2016-01-11 11:59:01 +0800

    Initial Commit

---
+++ Dummy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dummy
+//
+//  Created by Huan Lin on 1/11/16.
+//  Copyright © 2016 Huan Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
