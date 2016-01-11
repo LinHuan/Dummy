a8dda10fe9c808ab19ec135c7b9f62fd908eedcf
Huan Lin <lifesaglitch@qq.com>
2016-01-11 12:02:10 +0800

    first commit

---
+++ Dummy/EditProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MyProfileViewController.swift
+//  AppleFanboy
+//
+//  Created by Huan Lin on 1/11/16.
+//  Copyright © 2016 Huan Lin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class EditProfileViewController : UIViewController {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var verifyEmailButton: UIButton!
+    
+    
+    @IBOutlet weak var addressField: UITextField!
+    @IBOutlet weak var statusField: UITextField!
+    @IBOutlet weak var mottoField: UITextField!
+    
+    
+    
+    @IBAction func cancelAction(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func saveAction(sender: UIBarButtonItem) {
+    }
+    
+    
+        
+}
